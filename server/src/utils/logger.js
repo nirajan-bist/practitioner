ef3f950042a19f7f7e17c4f68bb6f1a05b28e9c6
Nirajan Bist <bistneon@gmail.com>
2022-09-24 20:28:56 +0545

    Added morgan logger for http requests

---
+++ server/src/utils/logger.js
@@ -0,0 +1,36 @@
+import winston, { format } from "winston";
+
+const logFormat = (params) => {
+  const { level, message, metadata } = params;
+  const { timestamp, context } = metadata || {};
+
+  return `${level} ${timestamp} ${context || ""}:: ${message}`;
+};
+
+const logger = new winston.createLogger({
+  format: format.combine(format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), format.metadata(), format.prettyPrint()),
+  transports: [
+    new winston.transports.Console({
+      format: format.combine(format.colorize(), format.printf(logFormat)),
+      level: "debug",
+    }),
+  ],
+});
+
+const createLogger = (context) => {
+  const childLogger = logger.child({ context });
+  const newLogger = (message) => childLogger.info(message);
+  newLogger.info = (message) => childLogger.info(message);
+  newLogger.warn = (message) => childLogger.warn(message);
+  newLogger.error = (message) => childLogger.error(message);
+  return newLogger;
+};
+
+// Object having write function for callback with morgan.
+export const writeStream = {
+  write(message) {
+    logger.info(message);
+  },
+};
+
+export default createLogger;
