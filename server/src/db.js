0f2ab358777d75d27e20dad6f33d912da7d8ff1b
Nirajan Bist <bistneon@gmail.com>
2022-09-29 17:33:56 +0545

    Added Additional knex config

--- server/src/db.js
+++ server/src/db.js
@@ -1,5 +1,27 @@
 import knex from "knex";
 import dbConfig from "./knexfile";
 
+import camelCaseKeys from "camelcase-keys";
+import snakeCase from "lodash/snakeCase";
+
+const additionalConfig = {
+  wrapIdentifier: (value, origImpl) => {
+    if (value === "*") {
+      return origImpl(value);
+    }
+
+    return origImpl(snakeCase(value));
+  },
+  postProcessResponse: (result) => {
+    if (Array.isArray(result)) {
+      if (result.length === 0 || !result[0] || typeof result[0] !== "object") {
+        return result;
+      } else {
+        return camelCaseKeys(result, { deep: true });
+      }
+    }
+  },
+};
+
 // Configure knex with db params set in knexfile.
-export default knex(dbConfig);
+export default knex({ ...dbConfig, ...additionalConfig });
