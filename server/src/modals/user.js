d58d9dced845891b254951423a0545ff94f1297b
Nirajan Bist <bistneon@gmail.com>
2022-09-29 23:16:16 +0545

    Added user modal and routes

--- server/src/modals/user.js
+++ server/src/modals/user.js
@@ -1,7 +1,22 @@
 import db from "../db";
 
-const userTable = db("user");
+const TABLE_NAME = "user";
 
-export function fetchAll() {
-  return userTable.select("id", "email", "createdAt");
+const qb = () => db(TABLE_NAME);
+
+export function fetchById(id) {
+  return qb().select("*").where("id", id);
+}
+export function fetchByEmailAndPassword(email, password) {
+  return qb()
+    .select("id", "email", "created_at")
+    .where("email", email)
+    .where("password", password)
+    .then(([result]) => result);
+}
+
+export function insert(data) {
+  return qb()
+    .insert(data, ["id", "email"])
+    .then(([result]) => result);
 }
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,8 +2,37 @@ import { Router } from "express";
 
 const app = Router();
 
-app.get("/", (req, res) => {
-  res.send("Test");
+import * as User from "../modals/user";
+
+app.post("/signin", async (req, res, next) => {
+  const data = req.body;
+  //TODO: Proper validation required!
+  if (!data?.email && !data?.password) {
+    return res.send("Please provide email and password!");
+  }
+
+  try {
+    const user = await User.fetchByEmailAndPassword(data.email, data.password);
+
+    if (!user) {
+      //TODO: Send user not found error
+      return res.status(404).send("User not found!");
+    }
+
+    res.send({ msg: `Loggeed in: ${user.email}`, user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/signup", async (req, res, next) => {
+  const data = req.body;
+  try {
+    const user = await User.insert(data);
+    res.send({ msg: `Welcome new user: ${user.email}`, user });
+  } catch (e) {
+    next(e);
+  }
 });
 
 export default app;
