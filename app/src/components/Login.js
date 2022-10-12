611601cb7339732feff9fc195cc5e094cee873b3
Nirajan Bist <bistneon@gmail.com>
2022-10-12 12:01:36 +0545

    Login component with formik

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,53 +1,84 @@
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import BsForm from "react-bootstrap/Form";
 
 import useDocumentTitle from "hooks/useDocumentTitle";
 
 import "./login.css";
+import { Link } from "react-router-dom";
+import { Formik, Field, Form } from "formik";
 
-const { Group, Label, Control } = Form;
+const { Group, Label, Control, Text } = BsForm;
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 export default function Login() {
   useDocumentTitle("Login");
 
-  return (
-    <div id="login-form" className="">
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
+  const FormikComponent = (props) => {
+    return (
       <Form className="form-signin">
         <div className="logo m-auto mb-3" />
         <h1 className="h3 mb-3 font-weight-normal text-center">
           Please sign in
         </h1>
-        <Group>
-          <Label htmlFor="inputEmail" visuallyHidden>
-            Email address
-          </Label>
-          <Control
-            type="email"
-            id="inputEmail"
-            className="form-control"
-            placeholder="Email address"
-            required
-            autofocus
-          />
-        </Group>
 
-        <Group className="mb-3">
-          <Label htmlFor="inputPassword" visuallyHidden>
-            Password
-          </Label>
-          <input
-            type="password"
-            id="inputPassword"
-            className="form-control"
-            placeholder="Password"
-            required
-          />
-        </Group>
+        <Field name="email">
+          {({ field }) => (
+            <Group className="mt-3">
+              <Label htmlFor="inputEmail" visuallyHidden>
+                Email address
+              </Label>
+              <Control
+                {...field}
+                type="email"
+                id="inputEmail"
+                className="form-control"
+                placeholder="Email address"
+                required
+              />
+            </Group>
+          )}
+        </Field>
+        <Field name="password">
+          {({ field }) => (
+            <Group className="mb-3">
+              <Label htmlFor="inputPassword" visuallyHidden>
+                Password
+              </Label>
+              <Control
+                {...field}
+                type="password"
+                id="inputPassword"
+                placeholder="Password"
+                required
+              />
+            </Group>
+          )}
+        </Field>
         <Button type="submit" variant="primary" size="lg" className="w-100">
-          Sign in
+          Sign Up
         </Button>
+
+        <Text>
+          Don't have an account? <Link to="/signup">Create new</Link>
+        </Text>
         <p className="mt-5 mb-3 text-muted text-center">&copy; 2021-2022</p>
       </Form>
+    );
+  };
+
+  return (
+    <div id="login-form" className="">
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {FormikComponent}
+      </Formik>
     </div>
   );
 }
