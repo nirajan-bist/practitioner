ea547a2ed07e6ad3d6faa14f54630a5ce0af22e4
Nirajan Bist <bistneon@gmail.com>
2022-10-15 11:28:54 +0545

    Added cors middleware in server

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import "./env";
 // Imports
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import routes from "./routes";
 import { authenticateRequest } from "./middlewares/auth";
 
@@ -11,6 +12,7 @@ const logger = createLogger("index");
 
 // Initialization
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny", { stream: writeStream }));
